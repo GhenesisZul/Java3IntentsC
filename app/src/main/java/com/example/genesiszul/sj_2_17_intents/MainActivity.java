08b35d92c14a705bb6d144cf8cc69d61f15565ef
ghenesiszul <tozulm@gmail.com>
2017-09-26 17:53:41 -0400

    commit inicial

--- app/src/main/java/com/example/genesiszul/sj_2_17_intents/MainActivity.java
+++ app/src/main/java/com/example/genesiszul/sj_2_17_intents/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.genesiszul.sj_2_17_intents;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -18,15 +19,12 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
 
-    private ImageButton ibAbreActivity;
-    private ImageButton ibEnviaDatos;
-    private ImageButton ibDevuelveDatos;
-    private ImageButton ibAbreMarcado;
-    private ImageButton ibLlamar;
-    private ImageButton ibAbrirGoogleMaps;
-    private ImageButton ibAbrirPaginaWeb;
-    private ImageButton ibCompartirImagen;
-    private ImageButton ibEnviarMail;
+    private ImageButton ibAbrirMarcado;
+    private ImageButton ibLLamar;
+    private ImageButton ibAbreGoogleMaps;
+    private ImageButton ibAbrePaginaWeb;
+    private ImageButton ibEnviaEmail;
+    private ImageButton ibComparteImagen;
 
     private Intent intent;
 
@@ -35,156 +33,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ibAbreActivity = (ImageButton) findViewById(R.id.ibAbreActivity);
-        ibEnviaDatos = (ImageButton) findViewById(R.id.ibEnviaDatos);
-        ibDevuelveDatos = (ImageButton) findViewById(R.id.ibDevueveDatos);
-        ibAbreMarcado = (ImageButton) findViewById(R.id.ibAbrirMarcado);
-        ibLlamar = (ImageButton) findViewById(R.id.ibLlamar);
-        ibAbrirGoogleMaps = (ImageButton) findViewById(R.id.ibAbreGoogleMaps);
-        ibAbrirPaginaWeb = (ImageButton) findViewById(R.id.ibAbrePaginaWeb);
-        ibCompartirImagen = (ImageButton) findViewById(R.id.ibComparteImagen);
-        ibEnviarMail = (ImageButton) findViewById(R.id.ibEnviaEmail);
+        ibAbrirMarcado = (ImageButton) findViewById(R.id.ibAbrirMarcado);
+        ibLLamar = (ImageButton) findViewById(R.id.ibLlamar);
+        ibAbreGoogleMaps = (ImageButton) findViewById(R.id.ibAbreGoogleMaps);
+        ibAbrePaginaWeb = (ImageButton) findViewById(R.id.ibAbrePaginaWeb);
+        ibEnviaEmail = (ImageButton) findViewById(R.id.ibEnviaEmail);
+        ibComparteImagen = (ImageButton) findViewById(R.id.ibComparteImagen);
 
-
-        ibAbreActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                abrirActivity();
-            }
-        });
-        ibEnviaDatos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enviarDatos();
-            }
-        });
-
-        ibDevuelveDatos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                devolverDatos();
-            }
-        });
-        ibAbreMarcado.setOnClickListener(new View.OnClickListener() {
+        ibAbrirMarcado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 abrirMarcadoTelefonico();
             }
         });
 
-        ibLlamar.setOnClickListener(new View.OnClickListener() {
+        ibLLamar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 llamar();
             }
-
-
-
-
         });
-        ibAbrirGoogleMaps.setOnClickListener(new View.OnClickListener() {
+
+        ibAbreGoogleMaps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 abrirGoogleMaps();
             }
         });
-        ibAbrirPaginaWeb.setOnClickListener(new View.OnClickListener() {
+
+        ibAbrePaginaWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 abrirPaginaWeb();
             }
         });
 
-        ibCompartirImagen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                compartirImagen();
-            }
-        });
-        ibEnviarMail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enviarMail();
-            }
-        });
-
 
     }
 
-
-    public void abrirActivity() {
-        intent = new Intent(MainActivity.this, SegundaActivity.class);
-        startActivity(intent);
-    }
-
-    public void enviarDatos() {
-        intent = new Intent(
-                getApplicationContext(),
-                SegundaActivity.class
-        );
-        intent.putExtra("dato1", "Luz");
-        intent.putExtra("dato2", "Genesis");
-        startActivity(intent);
-    }
-
-    public void devolverDatos() {
-        intent = new Intent(
-                getApplicationContext(),
-                SegundaActivity.class
-        );
-        intent.putExtra("mensaje", "Hola");
-        startActivityForResult(intent, 2);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 2) {
-            if (resultCode == RESULT_OK) {
-                String resultado = data.getStringExtra("respuesta").toString();
-                Toast.makeText(
-                        getApplicationContext(),
-                        "Respuesta: " + resultado,
-                        Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(
-                        getApplicationContext(),
-                        "Se cancelo la respuesta.",
-                        Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    public void abrirMarcadoTelefonico() {
+    public void abrirMarcadoTelefonico(){
         intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:61146624"));
         startActivity(intent);
     }
 
-    public void llamar() {
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
-       /* if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 123);
-        } else {
-            /*intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("tel:61146624"));
-            startActivity(intent);*/
+    public void  llamar(){
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CALL_PHONE);
 
-
-            Intent i = new Intent(android.content.Intent.ACTION_CALL,
-                    Uri.parse("tel:61146624"));
-                    startActivity(i);
-
-        //}
+        intent = new Intent(Intent.ACTION_CALL);
+        Uri.parse(("tel:61146624"));
+        startActivity(intent);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults){
+        switch (requestCode){
             case 123:
-                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    llamar();
-                } else {
+                if ((grantResults.length > 0) &&
+                        (grantResults[0]== PackageManager.PERMISSION_GRANTED)){
+                  llamar();
+                }else{
                     Log.d("TAG", "Permiso de llamada no concedido");
                 }
                 break;
@@ -193,44 +102,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void abrirGoogleMaps() {
+    public void abrirGoogleMaps(){
         intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("geo:-16.504014,-68.130906"));
+        intent.setData(Uri.parse("geo: -16.504014, -68.130906"));
         startActivity(intent);
     }
 
-    public void abrirPaginaWeb() {
+    public void abrirPaginaWeb(){
         intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("http://www.gdg.androidbolivia.com"));
+        intent.setData(Uri.parse("https://www.gdg.androidbolivia.com"));
         startActivity(intent);
     }
 
-    public void compartirImagen() {
-       /* intent = new Intent(Intent.ACTION_SEND);
-        Uri uri = Uri.parse(
-                "android.resource://com.miramicodigo.sj_a_1_17_java_3_intents/" + R.drawable.googlemaps);
-        intent.putExtra(Intent.EXTRA_STREAM, uri);
-        intent.setType("entrar/png");
-        startActivity(intent);*/
-
-    }
-
-    public void enviarMail() {
-        String[] TO = {"lizarraga.gux@gmail.com"};
-        String[] CC = {"androidlapaz@gmail.com"};
-        String asunto = "Correo Importante";
-        String contenido = "Este es una prueba de correo";
-
-        intent = new Intent(Intent.ACTION_SEND);
-        intent.setData(Uri.parse("mailto:"));
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_EMAIL, TO);
-        intent.putExtra(Intent.EXTRA_CC, CC);
-        intent.putExtra(Intent.EXTRA_SUBJECT, asunto);
-        intent.putExtra(Intent.EXTRA_TEXT, contenido);
-
-        startActivity(Intent.createChooser(intent, "Envia Correo"));
-    }
 
 
 }
